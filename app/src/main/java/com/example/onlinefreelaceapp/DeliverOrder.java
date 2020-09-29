2a1dc687acb68dffc9c79a764b04da1f809e8a7b
pramitha1166 <pramitha.wanigarathne@gmail.com>
2020-09-29 22:33:41 +0530

    4. From Pramitha | new items

--- app/src/main/java/com/example/onlinefreelaceapp/DeliverOrder.java
+++ app/src/main/java/com/example/onlinefreelaceapp/DeliverOrder.java
@@ -51,5 +51,17 @@ public class DeliverOrder extends AppCompatActivity {
         txtDeliverOrderDetails.setText(getIntent().getStringExtra("req"));
         deliverOrderId.setText("Order ID: #"+getIntent().getStringExtra("id"));
 
+
+        btnOrderResource.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String deliverDetails = txtDeliverOrderDetails.getText().toString();
+              //  String OrderId =
+
+            }
+        });
+
+
     }
 }
